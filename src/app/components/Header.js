e83c143e713d7b04667324e508e2177824c26808
a-student-account <mgillette002@student.butte.edu>
2025-11-10 04:17:50 +0000

    Add favicon, title, and logo

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,7 +1,9 @@
+import { BookOpenIcon } from "@heroicons/react/24/solid"
+
 export default function Header() {
   return (
     <header className="flex flex-col gap-y-6">
-      <h1 className="text-2xl fold-bold">My First React Page</h1>
+      <h1 className="text-2xl fold-bold inline text-blue-400">My First React Page is LIVE! <BookOpenIcon className = 'w-8 h-8 inline' /></h1>
       <h2>Here you find out information about my first React Page</h2>
     </header>
   )
--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,6 +1,8 @@
 import Button from './Button'
 import Tag from './Tag'
-
+import { AcademicCapIcon } from '@heroicons/react/24/solid'
+/*<div className="logo bg-indigo-600 rounded-full p-6 hover:scale-105 transition-all cursor-pointer"></div>
+*/
 const navigationButtons = [
   {
     title: 'Home',
@@ -17,7 +19,7 @@ const navigationButtons = [
 export default function Navbar() {
   return (
     <nav className="flex justify-between bg-indigo-50 p-6 items-center sticky top-0 shadow-lg">
-      <div className="logo bg-indigo-600 rounded-full p-6 hover:scale-105 transition-all cursor-pointer"></div>
+      <AcademicCapIcon className='w-10 h-10 text-indigo-600'/>
       <div className="flex gap-x-4 items-center">
         {navigationButtons.map(({ title, href, isNew}) => 
           <Button href = {href} key = {title} classNameProp='flex'>
